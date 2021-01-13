125fd1e43226c9b9ae9a7419b604ed4dcde53b36
Max Petretta <git@maxpetretta.com>
2021-01-13 00:10:58 -0500

    Added syntax highlighting via rehype-shiki

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
-const withMDX = require('@next/mdx')({
+const rehypeShiki = require('rehype-shiki')
+const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
-})
+  options: {
+    rehypePlugins: [
+      [
+        rehypeShiki,
+        {
+          theme: "./styles/monokai-pro.json",
+          useBackground: true,
+        },
+      ],
+    ],
+  },
+});
 
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'mdx'],
