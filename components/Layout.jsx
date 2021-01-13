e22455f8c0a4bfa4c768365a7014462b0ad5fa8e
Max Petretta <max.petretta@ge.com>
2021-01-13 00:10:58 -0500

    Added syntax highlighting via rehype-shiki

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,13 +1,20 @@
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import Header from './Header'
+import Footer from './Footer'
+import { MDXProvider } from '@mdx-js/react'
+
+const components = {
+  pre: props => <pre style={{background: props.style[0].split(":")[1]}}>{props.children}</pre>
+}
 
 export default function Layout({ children }) {
   return (
     <>
       <Header />
-      <main>
-        {children}
-      </main>
+      <MDXProvider components={components}>
+        <main>
+          {children}
+        </main>
+      </MDXProvider>
       <Footer />
     </>
   )
