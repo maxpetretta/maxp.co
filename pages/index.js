ac816f1164171e4d2575c244bdb215d9c7e075b7
Max Petretta <max.petretta@gmail.com>
2021-03-05 22:15:17 -0600

    Redesigned post cards, adjusted heading sizes

--- pages/index.js
+++ pages/index.js
@@ -13,14 +13,14 @@ export default function Home({ posts }) {
       <section>
         <h4>👋 Hey there!</h4>
         <h1>I'm Max Petretta,</h1>
-        <p>A <a href="https://github.com/maxpetretta">software engineer</a> living in Milwaukee, WI.  I work for GE Healthcare, where I design & build infrastructure for our cloud-hosted applications.  This is my personal site, where I experiment with new technology, and write about the <Link href="/blog"><a>topics that interest me</a></Link>.</p>
+        <p>A <a href="https://github.com/maxpetretta">software engineer</a> living in Milwaukee, WI.  I work for GE Healthcare, where I design & build infrastructure for our cloud-hosted applications.  This is my personal site, where I experiment with new technology, and write about the <Link href="/blog"><a>things that interest me</a></Link>.</p>
         <p>Want to get in touch?  You can find me on <a href="https://twitter.com/maxpetretta">Twitter</a>, or send me <Link href="/contact"><a>an email</a></Link>.</p>
       </section>
       <section>
-        <h2 className="mt-10">Recent Blog Posts</h2>
+        <h2 className="mt-10">Featured Posts</h2>
         <hr/>
         <div className="flex flex-wrap justify-center">
-          {posts.slice(0, 3).map(post => {
+          {posts.slice(0, 4).map(post => {
             return <Card key={post.slug} post={post} />
           })}
         </div>
