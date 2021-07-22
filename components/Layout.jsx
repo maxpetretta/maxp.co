0afebd10cb71f2fa0cb7368fd20579fb88f77654
Max Petretta <git@maxpetretta.com>
2021-07-21 23:55:08 -0500

    Various styling changes

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -9,22 +9,22 @@ const components = {
 export default function Layout({ children }) {
   return (
     <>
-    <div className="brand-gradient fixed top-0 w-full h-full -z-10"/>
-    <div className="p-2 md:p-6">
-      <div className="transition duration-300 w-full p-2 rounded-xl 
-      text-gray-900 bg-gray-100 dark:text-gray-300 dark:bg-gray-900">
-        <div className="max-w-5xl mx-auto">
-          <Header />
-          <MDXProvider components={components}>
-            <main className="max-w-3xl mx-auto text-lg">
-              {children}
-            </main>
-          </MDXProvider>
-          <Footer />
+      <div className="brand-gradient fixed top-0 w-full h-full -z-10"/>
+      <div className="p-2 md:p-6">
+        <div className="transition duration-300 w-full p-2 rounded-2xl 
+        text-gray-900 bg-gray-100 dark:text-gray-100 dark:bg-gray-900">
+          <div className="max-w-5xl mx-auto">
+            <Header />
+            <MDXProvider components={components}>
+              <main className="max-w-3xl mx-auto text-lg">
+                {children}
+              </main>
+            </MDXProvider>
+            <Footer />
+          </div>
         </div>
       </div>
-    </div>
-    <div className="brand-gradient fixed bottom-0 w-full h-full -z-10"/>
+      <div className="brand-gradient fixed bottom-0 w-full h-full -z-10"/>
     </>
   )
 }
