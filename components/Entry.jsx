d685348e059600c68f2c110415d8126b60ee921a
Max Petretta <git@maxpetretta.com>
2021-07-27 22:24:35 -0500

    Hide emoji on mobile

--- components/Entry.jsx
+++ components/Entry.jsx
@@ -5,7 +5,7 @@ export default function Entry({ post }) {
     <li className="transition duration-300 list-none">
       <Link href={"/blog/" + post.slug}>
         <a className="btn flex items-center py-0 m-0 rounded-none bg-transparent dark:bg-transparent">
-          <span className="ml-2 mr-6 text-lg md:text-2xl">{post.icon}</span>
+          <span className="hidden md:block ml-2 mr-6 text-lg md:text-2xl">{post.icon}</span>
           <div>
             <h3 className="mt-4 mb-0">{post.title}</h3>
             <p className="faint mt-0 mb-4">{post.description}</p>
