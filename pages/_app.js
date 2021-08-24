36a7450f959508364b467bb0b7d8cf4436d576fa
Max Petretta <git@maxpetretta.com>
2021-08-23 22:06:06 -0500

    Added Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -31,6 +31,20 @@ export default function App({ Component, pageProps }) {
       <Script
         src="/scripts/smoothscroll-anchor.min.js"
       />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        strategy="lazyOnload"
+      />
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
     </>
   )
 }
