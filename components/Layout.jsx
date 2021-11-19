154b24dea692c3756956f5cb74483b71211404aa
Max Petretta <git@maxpetretta.com>
2021-11-18 22:01:43 -0600

    Fix tab bar color in Safari

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -53,7 +53,7 @@ export default function Layout(props) {
           <meta property="article:published_time" content={meta.date} />
         )}
       </Head>
-      <div className="brand-gradient fixed bottom-0 w-full h-full -z-10" />
+      <div className="brand-gradient fixed top-0 w-full h-full -z-10" />
       <div className="flex min-h-screen p-2 md:p-6">
         <div className="flex-1 w-full p-3 rounded-2xl text-gray-900 bg-gray-100 dark:text-gray-100 dark:bg-gray-900">
           <div className="max-w-4xl mx-auto">
