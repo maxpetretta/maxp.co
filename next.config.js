9c9a32cedcf6d91c91e033576f6da1f2756baa74
Max Petretta <git@maxpetretta.com>
2022-08-07 19:17:59 -0400

    Replaced rehype-shiki w/ rehype-pretty-code

--- next.config.js
+++ next.config.js
@@ -1,16 +1,21 @@
-const rehypeShiki = require("rehype-shiki")
+const rehypePrettyCode = require("rehype-pretty-code")
+const fs = require("fs")
+
+const options = {
+  theme: JSON.parse(
+    fs.readFileSync(require.resolve("./styles/monokai-pro.json"), "utf-8")
+  ),
+  onVisitLine(node) {
+    if (node.children.length === 0) {
+      node.children = [{ type: "text", value: " " }]
+    }
+  },
+}
+
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
   options: {
-    rehypePlugins: [
-      [
-        rehypeShiki,
-        {
-          theme: "./styles/monokai-pro.json",
-          useBackground: true,
-        },
-      ],
-    ],
+    rehypePlugins: [[rehypePrettyCode, options]],
   },
 })
 
