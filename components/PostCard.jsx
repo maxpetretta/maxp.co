4179559d62bb9eb165a311139565056a62ec2d6b
Max Petretta <git@maxpetretta.com>
2022-08-24 15:26:39 -0400

    Switched twitt3r post's thumbnail to dalle version

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,12 +1,12 @@
-import Link from "next/link"
 import Image from "next/image"
+import Link from "next/link"
 
 export default function PostCard({ post }) {
   return (
     <Link href={"/blog/" + post.slug}>
       <a className="no-link card max-w-md transform flex-col transition duration-300 hover:scale-105 hover:shadow-xl md:w-5/12">
         <span className="chip absolute top-4 right-4 z-10">{post.tags[0]}</span>
-        <div className="relative mx-auto h-60 w-60 md:h-76 md:w-76">
+        <div className="relative mx-auto mt-3 h-60 w-60 md:h-76 md:w-76">
           <Image
             layout="fill"
             objectFit="contain"
