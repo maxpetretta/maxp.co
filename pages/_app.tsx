1e06fea62d411f2c371605c04a958320e699af1d
Max Petretta <git@maxpetretta.com>
2022-09-15 22:41:10 -0400

    Moved script tags from _app to _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,64 +1,13 @@
 import { ThemeProvider } from "next-themes"
 import type { AppProps } from "next/app"
-import Head from "next/head"
-import Script from "next/script"
 import "../styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        {/* Favicons */}
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicons/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicons/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicons/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicons/safari-pinned-tab.svg"
-          color="#4361a2"
-        />
-        <meta name="msapplication-TileColor" content="#c73156" />
-        <meta name="theme-color" content="#111827" />
-        {/* Preconnects */}
-        <link rel="preconnect" href="https://vitals.vercel-insights.com" />
-        <link rel="preconnect" href="https://www.googletagmanager.com" />
-        <link rel="preconnect" href="https://www.google-analytics.com" />
-      </Head>
       <ThemeProvider attribute="class" defaultTheme="light">
         <Component {...pageProps} />
       </ThemeProvider>
-      <Script src="/scripts/fade-in.ts" strategy="beforeInteractive" />
-      <Script src="/scripts/update-selector.ts" />
-      <Script src="/scripts/smoothscroll.min.js" />
-      <Script src="/scripts/smoothscroll-anchor.min.js" />
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
-      <Script id="gtag">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-            page_path: window.location.pathname,
-          });
-        `}
-      </Script>
     </>
   )
 }
