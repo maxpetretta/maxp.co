89733e4706d6290ecedc5bbdbb82519b95d292e3
Max Petretta <git@maxpetretta.com>
2024-01-06 11:55:14 -0500

    fix: transition flash

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,9 +1,36 @@
-<footer class="mb-8">
+<script>
+  import { Button } from "$/lib/components/ui/button"
+
+  const socials = [
+    {
+      name: "email",
+      href: "mailto:hello@maxpetretta.com",
+    },
+    {
+      name: "github",
+      href: "https://github.com/maxpetretta",
+    },
+    {
+      name: "farcaster",
+      href: "https://warpcast.com/maxp.eth",
+    },
+    {
+      name: "twitter",
+      href: "https://twitter.com/maxpetretta",
+    },
+    {
+      name: "linkedin",
+      href: "https://linkedin.com/in/maxpetretta",
+    },
+  ]
+</script>
+
+<footer class="-mx-4 mb-8">
   <address class="flex items-center justify-between not-italic">
-    <a href="mailto:hello@maxpetretta.com" target="_blank">email</a>
-    <a href="https://github.com/maxpetretta" target="_blank">github</a>
-    <a href="https://warpcast.com/maxp.eth" target="_blank">farcaster</a>
-    <a href="https://twitter.com/maxpetretta" target="_blank">twitter</a>
-    <a href="https://linkedin.com/in/maxpetretta" target="_blank">linkedin</a>
+    {#each socials as social}
+      <Button href={social.href} target="_blank" variant="ghost" class="text-xs text-zinc-500 dark:text-zinc-400">
+        {social.name}
+      </Button>
+    {/each}
   </address>
 </footer>
--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,27 +1,57 @@
 <script lang="ts">
+  import { Switch } from "$/lib/components/ui/switch"
   import { browser } from "$app/environment"
+  import { goto } from "$app/navigation"
   import { page } from "$app/stores"
+  import { Button } from "$lib/components/ui/button"
+  import { Command } from "lucide-svelte"
 
   function toggleTheme() {
     if (!browser) return
-    document.documentElement.classList.toggle("dark")
-
-    const isDark = document.documentElement.classList.contains("dark")
+    document.documentElement.classList.add("no-transition")
+    const isDark = document.documentElement.classList.toggle("dark")
     window.localStorage.setItem("theme", isDark ? "dark" : "light")
+    setTimeout(() => document.documentElement.classList.remove("no-transition"), 100)
+  }
+
+  async function handleNameClick() {
+    // eslint-disable-next-line svelte/valid-compile
+    if ($page.url.pathname === "/") {
+      await sharePageUrl()
+    } else {
+      await goto("/")
+    }
+  }
+
+  async function sharePageUrl() {
+     
+    const url = $page.url.href
+
+    try {
+      if (navigator.share) {
+        await navigator.share({ url })
+      } else {
+        await navigator.clipboard.writeText(url)
+      }
+    } catch (error) {
+      console.error(error)
+    }
   }
 </script>
 
 <header class="mt-8 md:mt-16">
   <nav class="flex items-center justify-between">
-    <a href="/">Max Petretta</a>
-    <div>
+    <Button href="/" onclick={handleNameClick} variant="ghost" class="-ml-4 text-base">Max Petretta</Button>
+    <div class="flex items-center space-x-5">
       <!-- eslint-disable-next-line svelte/valid-compile -->
       {#if $page.url.pathname !== "/"}
         <!-- eslint-disable-next-line svelte/valid-compile -->
-        <span>{$page.url.pathname}</span>
+        <Button onclick={sharePageUrl} variant="ghost">{$page.url.pathname}</Button>
       {/if}
-      <button onclick={toggleTheme}>☀️</button>
-      <button>⌘</button>
+      <Switch onCheckedChange={toggleTheme} />
+      <Button variant="outline" size="icon" class="transition-none">
+        <Command />
+      </Button>
     </div>
   </nav>
 </header>
