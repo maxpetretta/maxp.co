f4301a7c8065c764b67ce76c5f6d0f779a67d101
Max Petretta <git@maxpetretta.com>
2024-01-20 23:52:31 -0500

    fix: static builds

---
+++ src/routes/[post]/+page.server.ts
@@ -0,0 +1,4 @@
+export const entries = () => {
+  return [{ post: "test" }]
+}
+export const prerender = true
--- src/routes/uses/+page.md
+++
@@ -1,7 +0,0 @@
-# Uses
-
-**Here's some stuff I use**
-
-- SvelteKit
-- VS Code
-- Emojis 😎
