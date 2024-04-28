d30f09ad1657966de7ee02d7875c66c3677a1f83
Max Petretta <git@maxpetretta.com>
2024-04-27 20:45:36 -0400

    feat: enable anaytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,8 @@
+import { dev } from "$app/environment"
+import { inject } from "@vercel/analytics"
 import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
 
+inject({ mode: dev ? "development" : "production" })
 injectSpeedInsights()
 
 export const prerender = true
