0bffafb2fecd1b8b0a621dcfa4e8dc1c14856b86
Max Petretta <m@maxp.co>
2024-05-19 22:55:16 -0400

    feat: basic markdown rendering

---
+++ src/routes/[slug]/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  const { data } = $props()
+  const { metadata, content } = data
+</script>
+
+<article>
+  <h1>{metadata.title}</h1>
+  <p>Published: {metadata.date}</p>
+  <svelte:component this={content} />
+</article>
