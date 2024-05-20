ec5c6648504f90af15f59e8d1a0c9afbe404d1cd
Max Petretta <m@maxp.co>
2024-05-19 23:06:03 -0400

    fix: add entries function for post slugs

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,7 +2,7 @@ import { dev } from "$app/environment"
 import { inject } from "@vercel/analytics"
 import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit"
 
+export const prerender = true
+
 inject({ mode: dev ? "development" : "production" })
 injectSpeedInsights()
-
-export const prerender = true
